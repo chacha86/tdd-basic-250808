5c0e13c00906d85679f65a462a8e3c90de29065e
chacha86 <taejincha0619@gmail.com>
2025-08-08 14:28:29 +0900

    step-3 blue

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -2,16 +2,6 @@ package com.back;
 
 public class App {
     public int plus(int num1, int num2) {
-
-        if(num1 == 1 && num2 == 2) {
-            return 3;
-        }
-
-        if(num1 == 0 && num2 == 5) {
-            return 5;
-        }
-
-
-        return 30;
+        return num1 + num2;
     }
 }
