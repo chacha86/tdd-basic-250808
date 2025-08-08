29ab85a6ef9dea14c74192f871bfef547b2900cb
chacha86 <taejincha0619@gmail.com>
2025-08-08 14:17:13 +0900

    step-2 green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -2,6 +2,11 @@ package com.back;
 
 public class App {
     public int plus(int num1, int num2) {
-        return 3;
+
+        if(num1 == 1 && num2 == 2) {
+            return 3;
+        }
+
+        return 30;
     }
 }
